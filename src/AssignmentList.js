c8fef81a068e578de2c8b5fa89523b8dcb1c8069
AbhinavCh <abhinavch16@gmail.com>
2021-12-02 22:44:45 +0530

    Added edit button

--- src/AssignmentList.js
+++ src/AssignmentList.js
@@ -1,26 +1,34 @@
 import { Link } from "react-router-dom";
+import write from './write.png'
 
 const AssignmentList = (props) => {
 
     const assignments = props.assignments
     const title = props.title
 
-    return ( 
+    return (
         <div className="assignment-list">
             <h2>{title}</h2>
             {assignments.map((assignment) => (
                 <div className="assignment-preview" key={assignment.activity_id}>
-                    <Link to={'/assignments/'+assignment.activity_id}>
+                    <div className="assignment-preview-header">
                         <div className="career-name">
                             {assignment.careerName}
                         </div>
-                        <h2>{assignment.title}</h2>
-                        <p>Description: {assignment.description}</p>
-                    </Link>
+                        {/* <Link to={'/assignments/' + assignment.activity_id}> */}
+                        <Link to={'/create'}>
+                            <div className="assignment-edit">
+                                <img src={write} />
+                            </div>
+                        </Link>
+                    </div>
+                    <h2>{assignment.title}</h2>
+                    <p>Description: {assignment.description}</p>
+
                 </div>
             ))}
         </div>
-     );
+    );
 }
- 
+
 export default AssignmentList;
